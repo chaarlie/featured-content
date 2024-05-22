7af2bbfb0b175adf670b36aa5d87a496bdf7581e
Carlos De la Rosa <chaarlie@hotmail.com>
2024-05-22 12:04:04 -0400

    removing unsed services

--- featured-content-api/apps/featured-content/src/featured-content.module.ts
+++ featured-content-api/apps/featured-content/src/featured-content.module.ts
@@ -3,10 +3,7 @@ import { CqrsModule } from '@nestjs/cqrs';
 import { Module } from '@nestjs/common';
 
 import { GetFeaturedContentQueryHandler } from './application/get-featured-content.query-handler';
-import { GetTranslatedTextQueryHandler } from './application/get-translated-text.query-handler';
-import { GetFeaturedTranslatedContentQueryHandler } from './application/get-featured-translated-content.query-handler';
 import { ClientsModule, Transport } from '@nestjs/microservices';
-import { LibreTranslateManagerModule } from '../../../libs/libretranslate-manager/src/libretranslate-manager.module';
 import { ProxyWikimediaRequestModule } from '../../../libs/proxy-wikimedia-request/src/proxy-wikimedia-request.module';
 import { FeaturedContentController } from './interface/featured-content.controller';
 import {
@@ -20,14 +17,11 @@ export const TRANSLATION_SERVICE = 'TRANSLATION_SERVICE';
 
 const application = [
   GetFeaturedContentQueryHandler,
-  GetFeaturedTranslatedContentQueryHandler,
-  GetTranslatedTextQueryHandler,
 ];
 
 @Module({
   imports: [
     TranslationModule,
-    LibreTranslateManagerModule,
     ProxyWikimediaRequestModule,
     CqrsModule,
     ClientsModule.register([
